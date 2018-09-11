a513e48355a75a84b28e7da66e10bf68ef4b100f
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-11 18:11:43 +0300

    -_-

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const steak = [];
+
+  for (let i = 0; i < str.length; i++){
+    for (let j = 0; j < bracketsConfig.length; j++){
+
+      if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]){
+        (steak[steak.length - 1] !== str[i]) ? steak.push(str[i]) : steak.pop();
+        break;
+      }
+
+
+      if (str[i] === bracketsConfig[j][0]){
+        steak.push(str[i]);
+        break;
+      }
+
+
+      if (str[i] === bracketsConfig[j][1]){
+        if (steak.length === 0) return false;
+        if (steak[steak.length - 1] !== bracketsConfig[j][0]) return false;
+        steak.pop();
+        break;
+      }
+
+    }
+
+  }
+
+  if (steak.length > 0) return false;
+  return true;
 }
